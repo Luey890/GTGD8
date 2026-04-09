ae8db47f8b50291271d312ba38147fee20b445fb
Luey890 <114149842+Luey890@users.noreply.github.com>
2026-04-09 13:17:59 +1000

    Commit2
    Setup database python script, login webpage

---
+++ db.py
@@ -0,0 +1,31 @@
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def GetDB():
+    db = sqlite3.connect("C:\\Users\\hhmin\\OneDrive\\Documents\\GTGD8\\github\\.database\\gtg.db")
+    db.row_factory = sqlite3.Row
+
+    return db
+
+def GetAllGuesses():
+    db = GetDB()
+    Guesses = db.execute("SELECT * FROM Guesses").fetchall()
+    db.close()
+    return Guesses
+
+def CheckLogin(username, password):
+
+    db = GetDB()
+
+    # Ask the database for a single user matching the provided name
+    user = db.execute("SELECT * FROM Users WHERE username=?", (username,)).fetchone()
+
+    # Do they exist?
+    if user is not None:
+        # OK they exist, is their password correct
+        if check_password_hash(user['password'], password):
+            # They got it right, return their details 
+            return user
+        
+    # If we get here, the username or password failed.
+    return None
--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, session
+import db
 
 app = Flask(__name__)
 app.secret_key = "gtg"
 
 @app.route("/")
 def Home():
-    return render_template("index1.html")
+    guessData = db.GetAllGuesses() # Note: the new line
+    return render_template("index1.html", guesses=guessData) #Note: passing data
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+
+##################################
+### New code starts here
+##################################
+
+    # They sent us data, get the username and password
+    # then check if their details are correct.
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+
+        # Did they provide good details
+        user = db.CheckLogin(username, password)
+        if user:
+            # Yes! Save their username and id then
+            session['id'] = user['id']
+            session['username'] = username
+
+            # Send them back to the homepage
+            return redirect("/")
+
+
+##################################
+### New code ends here
+##################################
+
+    return render_template("login.html")
+
+
 
 app.run(debug=True, port=5000)
